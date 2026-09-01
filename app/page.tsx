bdf74d56a755c7022e08a73b711e1fc04edfcd2f
kusahl <dahalkushal00@gmail.com>
2026-09-01 12:36:08 +0545

    Update music courses data and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses"
+import WhyChooseUse from "@/components/WhyChooseUse"
 
 export default function Home() {
   return (
     <>
     <HeroSection/>
+    <FeaturedCourses/>
+    <WhyChooseUse/>
     </>
   );
 }
